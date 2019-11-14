f68756db4929d8d242f42ea6f67e790e1172733e
Nathan Langridge <nathanlangridge@hotmail.com>
2019-11-14 10:53:16 +0000

    Created Player Class

---
+++ src/main/java/player/Player.java
@@ -0,0 +1,21 @@
+package player;
+
+public abstract class Player {
+
+    private String name;
+    private int healthPoints;
+
+    public Player(String name, int healthPoints) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHealthPoints() {
+        return this.healthPoints;
+    }
+
+}
